a455727d9a0833075582d4a8b48f34e7aa6d8b6e
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2021-06-03 20:19:23 +0000

    testing skeleton

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -3,7 +3,16 @@
 
 import unittest
 
+from ops.testing import Harness
+from charm import CassandraStressOperatorCharm
+
 
 class TestCharm(unittest.TestCase):
+    def setUp(self):
+        self.harness = Harness(CassandraStressOperatorCharm)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.set_leader(True)
+        self.harness.begin_with_initial_hooks()
+
     def test_pass(self):
         pass
